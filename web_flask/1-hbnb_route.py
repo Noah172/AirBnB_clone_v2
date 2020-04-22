f47a02a8df5cd03387fa1d1441641ff268332f87
noah172 <jsebastian.garcia1995@gmail.com>
2020-04-22 17:11:11 -0500

    task 0

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -14,5 +14,5 @@ def hello_world():
 def hbnb():
     return "HBNB"
 
-    if __name__ == '__main__':
-            app.run(host="0.0.0.0", port="5000")
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port="5000")
