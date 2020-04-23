753bf28b9b9b86fd53f78ab0a16bd936b9643cc5
noah172 <jsebastian.garcia1995@gmail.com>
2020-04-23 10:59:41 -0500

    importing states and storage

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,6 +2,8 @@
 """ This script starts a Flask web application. """
 from flask import Flask
 from flask import render_template
+from models import storage
+from models.state import State
 
 app = Flask(__name__)
 
